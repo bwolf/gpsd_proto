c750490fc13d7951b0df7b6030dd0a290f95fedf
Marcus Geiger <git013@teufelsgraben.xyz>
2023-11-01 09:42:45 +0100

    Update Nix flake to use stable toolchain

--- flake.nix
+++ flake.nix
@@ -1,29 +1,56 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlays; };
+  outputs = { self, nixpkgs, naersk, fenix }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
 
-        rust = pkgs.rust-bin.stable."1.56.1";
-        rust-bin = rust.default;
-        rust-src = rust.rust-src;
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            rust.default
-            rust.rust-src
-            pkgs.rust-analyzer
-          ];
+      toolchain = fenix.packages.${system}.fromToolchainName {
+        name = "stable";
+        sha256 = "sha256-rLP8+fTxnPHoR96ZJiCa/5Ans1OojI7MLsmSqR2ip8o=";
+      };
 
-          RUST_BACKTRACE=1;
-          RUST_SRC="${rust.rust-src}/lib/rustlib/src/rust/library";
-        };
-      });
+      package-name = (naersk.lib.${system}.override {
+        inherit (toolchain) cargo rustc;
+      }).buildPackage { src = ./.; };
+
+    in {
+
+      packages.${system} = {
+        default = package-name;
+        inherit package-name;
+      };
+
+      devShells.${system}.default = nixpkgs.legacyPackages.${system}.mkShell {
+        packages = [
+          (toolchain.withComponents [
+            "cargo"
+            "rustc"
+            "rust-src"
+            "rustfmt"
+            "clippy"
+          ])
+
+          fenix.packages.${system}.rust-analyzer
+
+          pkgs.cargo-audit
+          pkgs.cargo-bloat
+          pkgs.cargo-outdated
+          pkgs.cargo-release
+          pkgs.cargo-watch
+        ];
+        RUST_BACKTRACE = 1;
+      };
+    };
 }
